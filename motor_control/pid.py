8cb6b1a1772e1a5dfb88aa7c4b98711f964ad6c8
Yael Ben Shalom <yael.bshalom@gmail.com>
2021-10-28 15:53:15 -0500

    update odrive config

--- motor_control/pid.py
+++ motor_control/pid.py
@@ -1,9 +1,18 @@
-# import time
+import time
 
 class PID:
 
-    def __init__(self, Kp = 0.5, Ki = 0.5, Kd = 0.1, SetPoint = 1, dt = 0.1):
-
+    def __init__(self, Kp=0.5, Ki=0.5, Kd=0.1, SetPoint=1, dt=0.1):
+        """ Initialize the PID controller gains.
+        Input:
+          Kp                Proportional gain
+          Kp                Proportional gain
+          Kp                Proportional gain
+          SetPoint          Desired position
+          dt                Time step
+        Output:
+          None
+        """
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
@@ -14,7 +23,8 @@ class PID:
         self.clear()
 
     def clear(self):
-        
+        """ Clear the controller values
+        """
         self.integral = 0.0
         self.pre_err = 0.0
         self.feedback_value = 0.0
@@ -22,7 +32,8 @@ class PID:
         self.Yp = 0.0
         
     def compute(self, feedback_value = None):
-
+        """ Compute the control output
+        """
         err, output, derivative = 0, 0, 0
         
         if(feedback_value is not None):
@@ -33,16 +44,14 @@ class PID:
         derivative = (err - self.pre_err) / self.dt
         output = (self.Kp * err + self.Ki * self.integral + self.Kd * derivative)
 
-
         self.pre_err = err
         self.feedback_value = output
 
         return output
     
-    
-    # if you don't need lowpassFilter, you can delete the code below
     def lowpassFilter(self, X, beta):
-    
+        """ Compute the lowpass filter
+        """
         Y = beta * X + (1 - beta) * self.Yp
         self.Yp = Y
 
